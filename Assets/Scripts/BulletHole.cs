9c315bed5fdb5267bf52e4d12b1be6f3c20adc67
KMoon222 <ka539608@ucf.edu>
2026-09-16 16:47:48 -0400

    basic bullet hole instantiation

---
+++ Assets/Scripts/BulletHole.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.Assemblies;
+
+public class BulletHole : MonoBehaviour
+{
+
+    private GameObject side1;
+    private GameObject side2;
+    private GameObject side3;
+
+
+
+
+
+
+
+
+    void Update()
+    {
+        
+
+
+
+    }
+}
--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -1,17 +1,45 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerCharacter : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public GameObject BulletHole;
+    private GameObject currentBullethole;
+    private GameObject side1;
+
+    [SerializeField] private InputActionAsset inputActions;
+    private InputAction interactAction;
+    private InputAction positionAction;
+
+    private Vector2 tapPosition;
+
+    private void Awake()
+    {
+        interactAction = inputActions.FindAction("Tap");
+        positionAction = inputActions.FindAction("Position");
+
+        side1 = GameObject.Find("Side1");
+
+    }
+
     void Start()
     {
         // Starting health
     }
 
-    // Update is called once per frame
     void Update()
     {
         // Decrease health over time in bosss fights
+
+        if (interactAction.triggered)
+        {
+            tapPosition = Camera.main.ScreenToWorldPoint(positionAction.ReadValue<Vector2>());
+
+            GameObject currentBullethole = Instantiate(BulletHole, tapPosition, Quaternion.identity);
+
+
+        }
 
     }
 }
